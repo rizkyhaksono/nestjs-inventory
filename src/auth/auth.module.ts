847218a81a88521eb2c7cbfba3a1281c05240137
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-28 22:05:37 +0700

    feat(auth): change register async

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,18 +6,27 @@ import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserModule } from 'src/user/user.module';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.jwtSecret,
-      signOptions: { expiresIn: '24h' },
+    // JwtModule.register({
+    //   secret: process.env.jwtSecret,
+    //   signOptions: { expiresIn: '24h' },
+    // }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwtSecret'),
+        signOptions: { expiresIn: '24h' },
+      }),
     }),
     UserModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
